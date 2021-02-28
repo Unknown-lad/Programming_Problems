a8ee0db739bf34ae1e0cac7041f01b514b4e9384
Souvik-Projects-03-07-2001 <gitprojects12@gmail.com>
2021-02-28 10:09:09 +0530

    2 probelms :(

--- C++/Basics/60_addition_of_binary.cpp
+++ C++/Basics/60_addition_of_binary.cpp
@@ -3,26 +3,27 @@ using namespace std;
 
 int main(){
 
-    int arr[10],arr2[10];
-    int len,i;
-    cout<<"Enter the lenght of array: ";
-    cin>>len;
-    cout<<"Input the 1st binary number: ";
-    for(i=0;i<len;i++){
-        cin>>arr[i];
-    }
-    
-    cout<<"Input the 2nd binary number: ";
-    for(i=0;i<len;i++){
-        cin>>arr2[i];
-    }
-    
-    for(i=0;i<len;i++){
-        if(arr[i]==arr2[i]){
-            
-        }
-    }
+    long n1,n2;
+    int i=0,r=0;
+    int sum[30];
+    cout<<"Enter 1st binary number: ";
+    cin>>n1;
+    cout<<"Enter 2nd binary number: ";
+    cin>>n2;
 
-    
+    while(n1!=0 || n2!=0){
+        sum[i++]=(int)((n1%10+n2%10)%2);
+        r=(int)((n1%10+n2%10)/2);
+        n1=n1/10;
+        n2=n2/10;
+    }
+    if(r!=0){
+        sum[i++]=r;
+    }
+    --i;
+    cout<<"sum of binary number: ";
+    while(i>=0){
+        cout<<sum[i--]<<" ";
+    }
 return 0;
 }
